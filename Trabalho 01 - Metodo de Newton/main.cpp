914d61319d7e761a469f9a2acf7c50d2d57e1582
tfraxe <tfraxe@gmail.com>
2018-10-09 17:01:07 -0300

    Adicionado, em main.cpp, testes do novo método de gerar derivada

--- Trabalho 01 - Metodo de Newton/main.cpp
+++ Trabalho 01 - Metodo de Newton/main.cpp
@@ -6,13 +6,19 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	vector<double> coef{1, 1, 2}; //Coeficiente do polinômio. A primeira posição corresponde ao coeficiente de maior grau. 
+	vector<double> coef{1, 1, 2, 2.5, 4.57}; //Coeficiente do polinômio. A primeira posição corresponde ao coeficiente de maior grau. 
 	try{
-		Polinomio teste(2, coef);
+		Polinomio teste(4, coef);
 		double resultado = teste.calcular(2);
 		cout << resultado << endl;
 		cout << teste;
 		cout << "P(x) = " << teste << ". " << "P(2) = " << resultado << endl;
+
+		Polinomio derivada = teste.gerarDerivada();
+
+		cout << "Derivada de " << teste << ": " << derivada << endl;
+
+		cout << "Valor de 2 na derivada: " << derivada.calcular(2) << endl;
 	} catch (const char* msg) {
 		cerr << msg << endl;
 	}
